38a9836c10b1660192c9d8f8bf70d9aa6e0dfbf4
FatimaMubeenSh <shfatimamubeen@gmail.com>
2024-11-05 13:36:07 +0500

    Project 6 Temperature Controller Completed

--- projects-sandbox/src/App.jsx
+++ projects-sandbox/src/App.jsx
@@ -3,7 +3,8 @@ import "./App.css";
 // import RandomizeColors from "./projects/02-randomize-colors/RandomizeColors";
 // import LikePhotoApp from "./projects/03-like-photo-app/LikePhotoApp";
 // import TestimonialsApp from "./projects/04-testimonials-app/TestimonialsApp";
-import AlertsDisplay from "./projects/05-alerts-display/AlertsDisplay";
+// import AlertsDisplay from "./projects/05-alerts-display/AlertsDisplay";
+import TemperatureController from "./projects/06-temperature-controller/TemperatureController";
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       {/* <RandomizeColors /> */}
       {/* <LikePhotoApp /> */}
       {/* <TestimonialsApp /> */}
-      <AlertsDisplay />
+      {/* <AlertsDisplay /> */}
+      <TemperatureController />
     </>
   );
 }
