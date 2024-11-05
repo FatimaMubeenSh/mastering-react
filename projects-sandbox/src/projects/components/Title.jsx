b0ec184ac378f27503e816f233399b66441c191a
FatimaMubeenSh <shfatimamubeen@gmail.com>
2024-11-05 12:44:51 +0500

    Refactored Title Component

--- projects-sandbox/src/projects/components/Title.jsx
+++ projects-sandbox/src/projects/components/Title.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
-export default function Title({ custom_class, text }) {
-  return <h1 className={!custom_class ? "container text-center" : custom_class}>{!text ? "Title" : text}</h1>;
+export default function Title({ custom_class, text = "Title" }) {
+  return <h1 className={`title ${!custom_class ? "container text-center" : custom_class}`}>{text}</h1>;
 }
